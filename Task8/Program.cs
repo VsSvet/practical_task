1e0195b1f698c4af0e67db65c6874c88dba0bade
Vostrova Svetlana <masuanuamasuanua1@gmail.com>
2023-01-26 14:47:35 +0300

    add Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+
+int count = 1;
+string numbers = "";
+
+while(count < A+1)
+{
+    if(count%2 == 0)
+    {
+        numbers += $"{count} ";
+    }
+    count += 1;
+}
+
+Console.Write(numbers);
