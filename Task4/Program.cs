73f4bb00bc35d9ed7ff06ef92c84c624dfbce303
Vostrova Svetlana <masuanuamasuanua1@gmail.com>
2023-01-26 12:50:06 +0300

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите  второе число: ");
+int B = int.Parse(Console.ReadLine());
+Console.Write("Введите  третье число: ");
+int C = int.Parse(Console.ReadLine());
+
+int max_number = A;
+
+if(A > max_number)
+{
+    max_number = A;
+}
+
+if(B > max_number)
+{
+    max_number = B;
+}
+
+if(C > max_number)
+{
+    max_number = C;
+}
+
+Console.WriteLine($"Максимальное число {max_number}");
