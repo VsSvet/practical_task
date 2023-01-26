8052224a0d17ff1bba99464ca60c5d9a93258baa
Vostrova Svetlana <masuanuamasuanua1@gmail.com>
2023-01-26 13:54:13 +0300

    add Task06

---
+++ Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+
+if(A%2 == 0)
+{
+    Console.WriteLine($"Число {A} четное");
+}
+else
+{
+    Console.WriteLine($"Число {A} нечетное");
+}
