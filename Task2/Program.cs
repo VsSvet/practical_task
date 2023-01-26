7ea40b25be1b392f48a173bd3e9626c55bf36e2f
Vostrova Svetlana <masuanuamasuanua1@gmail.com>
2023-01-26 12:38:22 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int B = int.Parse(Console.ReadLine());
+
+if(A>B)
+{
+    Console.WriteLine($"Число {A} больше числа {B}");
+}
+
+if(A<B)
+{
+    Console.WriteLine($"Число {B} больше числа {A}");
+}
